03bce3adf9709ebc5053d3bdf2c6f2ffcfb34fd7
dakshs-07 <dakshpinjore@gmail.com>
2026-03-21 18:30:00 +0530

    fixed navbar spacing

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
     const linkstyle = "hover:underline hover:opacity-70"
   return (
     <div>
-        <div className='bg-amber-400 flex justify-center items-center gap-8'>
+        <div className='bg-amber-400 flex justify-center items-center gap-8 py-1'>
             <Link href="/" className={linkstyle}>Algbera 1</Link>
             <Link href="/" className={linkstyle}>Algebra 2</Link>
             <Link href="/" className={linkstyle}>Geometry</Link>
