8d58db73327f0cc90e6cf62eea68fe02031db48f
dakshs-07 <dakshpinjore@gmail.com>
2026-03-21 18:57:39 +0530

    added footer

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Montserrat, Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const montserrat = Montserrat({
   variable: "--font-montserrat",
@@ -28,6 +29,7 @@ export default function RootLayout({ children }) {
       <body className="min-h-full flex flex-col">
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ components/Footer.jsx
@@ -0,0 +1,27 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+function Footer() {
+    const linkhover = "hover:underline hover:text-amber-200";
+  return (
+    <div className="bg-black text-white py-5">
+      <div className="flex justify-around max-h-[330px] py-1">
+        <Image src="/vercel.svg" alt="logo" width={100} height={100} />
+        <div className="flex flex-col justify-center items-center">
+          <Link href="/" className={linkhover}>Algebra 1</Link>
+          <Link href="/" className={linkhover}>Algebra 2</Link>
+          <Link href="/" className={linkhover}>Geometry</Link>
+        </div>
+        <div className="flex flex-col justify-center items-center">
+          <Link href="/" className={linkhover}>Algebra 1</Link>
+          <Link href="/" className={linkhover}>Algebra 2</Link>
+          <Link href="/" className={linkhover}>Geometry</Link>
+        </div>
+      </div>
+      <p className="text-sm text-center mt-3">Math Notes CC 2026</p>
+    </div>
+  );
+}
+
+export default Footer
