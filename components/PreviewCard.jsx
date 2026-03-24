8e6f0c3599d77f369ff60e0a9f13271f416e0b7e
dakshs-07 <dakshpinjore@gmail.com>
2026-03-24 17:09:29 +0530

    added preview card structure

--- components/PreviewCard.jsx
+++ components/PreviewCard.jsx
@@ -7,7 +7,7 @@ function PreviewCard({href, title, desc, children, preview}) {
         <Link href={href} className='cursor-pointer'>
         {children}</Link>
 
-        <div className='absolute left-15 top-full mt-2 w-72 p-4 opacity-0 group-hover:opacity-100 translate-y-2 group-hover:translate-y-0 transition-all duration-200 rounded-2xl shadow-xl border bg-white z-50'>
+        <div className='absolute left-15 top-full mt-2 w-72 p-4 opacity-0 group-hover:opacity-100 translate-y-2 group-hover:translate-y-0 transition-all duration-200 rounded-2xl shadow-xl bg-white z-50'>
             {preview && <div>{preview}</div>}
             <h4 className='font-semibold text-sm'>{title}</h4>
             <p className='text-xs text-gray-600'>{desc}</p>
