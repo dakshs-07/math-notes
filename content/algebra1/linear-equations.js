4693065fdb2c1f4119e8aff6c40e8130e465d726
dakshs-07 <dakshpinjore@gmail.com>
2026-03-24 19:50:31 +0530

    added content section for all the data

---
+++ content/algebra1/linear-equations.js
@@ -0,0 +1,15 @@
+const linearEquations = {
+    title: "Linear Equations",
+    sections: [
+        {
+            heading: "Form",
+            content: "y = mx + b"
+        },
+        {
+            heading: "Slope Intercept from",
+            content: "y_1 = x^2"
+        }
+    ],
+}
+
+export default linearEquations;
