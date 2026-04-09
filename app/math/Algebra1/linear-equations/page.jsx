1074f2ae4bb3148955cf92bd5bc65940a3f64c13
dakshs-07 <dakshpinjore@gmail.com>
2026-04-09 20:58:15 +0530

    added tailwind classes for notes styling

--- app/math/Algebra1/linear-equations/page.jsx
+++ app/math/Algebra1/linear-equations/page.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function page() {
   return (
-    <div>hi!</div>
+    <div className='mx-4'>
+      <div className='my-5'>
+        <h1 className='text-4xl text-center mb-4'>Linear Equations</h1>
+      </div>
+      <p>This is what we do do do do do do</p>
+    </div>
   )
 }
 
---
+++ app/math/Algebra1/quadratic-equations/page.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div className='mx-4 math-content'>
+      <div className='my-5'>
+        <h1 className='text-4xl text-center'>Quadratic Functions</h1>
+      </div>
+      <h2>Word problem solving:</h2>
+        <p>
+        <br />
+        <ul>
+            <li>Solving for anything maximum/minimum (max/min height): find the <b>vertex</b></li>
+            <li>Solving for when something hits the ground after being thrown: solve for the <b>x-intercepts</b></li>
+            <li>Solving for initial height: <b>y-intercept</b></li>
+            <li>Finding the initial velocity: <b>the b value</b></li>
+        </ul>
+      </p>
+    </div>
+  )
+}
+
+export default page
