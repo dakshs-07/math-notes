57c06917b1d09a11c06b783ac6e8175898f3d8ba
dakshs-07 <dakshpinjore@gmail.com>
2026-04-09 14:53:04 +0530

    added quokka to go with leebit

--- components/ComingSoon.jsx
+++ components/ComingSoon.jsx
@@ -1,8 +1,10 @@
+import Image from "next/image";
 import React from "react";
 
 function ComingSoon() {
   return (
     <div className="fixed inset-16 z-50 flex items-center justify-center bg-white/10 backdrop-blur-xs px-10">
+      {/* <Image src="/quokka.jpg" alt="quokka to go with leebit coz minsung" height={200} width={200}/> */}
       <h1 className="text-white text-5xl tracking-widest">
         Coming soon fellas hang in tight
       </h1>
