f7bb7ab2f8569561bc25745dc58fac0eb8a3a6bb
dakshs-07 <dakshpinjore@gmail.com>
2026-04-09 14:18:25 +0530

    replaced hardcoreded links for alg2 and geometry page, and added 5 topics each

--- app/math/Algebra2/page.jsx
+++ app/math/Algebra2/page.jsx
@@ -1,44 +1,39 @@
 import PageWrapper from "@/components/PageWrapper";
+import PreviewCard from "@/components/PreviewCard";
 import Link from "next/link";
 import React from "react";
 
 function page() {
   const link = "hover:text-pink-700 hover:bg-pink-100 px-1";
+  const slugify = (text) => text.toLowerCase().replace(/\s+/g, "-")
+  const basePath = "/math/Algebra2"
+  const topics = [
+    "Quadratics",
+    "Trigonometry",
+    "Exponential Functions",
+    "Rational Functions",
+    "Radical Functions"
+  ]
   return (
-    <PageWrapper
-      gradient="from-pink-100 to-rose-300"
-      fontColor="text-rose-900"
-    >
-      <div className="my">
+    <PageWrapper gradient="from-pink-100 to-rose-300" fontColor="text-rose-900">
+      <div>
         <h1 className="text-5xl text-center tracking-wide py-6">Algebra 2</h1>
         <div className="flex justify-around">
           <div className="flex flex-col justify-center items-center gap-2">
             <h1 className="text-2xl my-5">TOPICS</h1>
-            <Link href="/linear-equations" className={link}>
-              Linear Equations
-            </Link>
-            <Link href="/linear-equations" className={link}>
-              Linear Equations
-            </Link>
-            <Link href="/linear-equations" className={link}>
-              Linear Equations
-            </Link>
-            <Link href="/linear-equations" className={link}>
-              Linear Equations
-            </Link>
-            <Link href="/linear-equations" className={link}>
-              Linear Equations
-            </Link>
-            <Link href="/linear-equations" className={link}>
-              Linear Equations
-            </Link>
-            <Link href="/linear-equations" className={link}>
-              Linear Equations
-            </Link>
+            {topics.map((topic) => (
+              <Link href={`${basePath}/${slugify(topic)}`} key={topic}>
+                <PreviewCard title={topic} preview={<img src="/file.svg" />}>
+                  <span className="link">{topic}</span>
+                </PreviewCard>
+              </Link>
+            ))}
           </div>
           <div className="flex flex-col items-center gap-2">
             <h1 className="text-2xl my-5">QUICK LINKS</h1>
-            <Link href="/" className={link}>Cheatsheets</Link>
+            <Link href="/" className={link}>
+              Cheatsheets
+            </Link>
           </div>
         </div>
       </div>
--- app/math/Geometry/page.jsx
+++ app/math/Geometry/page.jsx
@@ -1,40 +1,36 @@
 import PageWrapper from "@/components/PageWrapper";
+import PreviewCard from "@/components/PreviewCard";
 import Link from "next/link";
 import React from "react";
 
 function page() {
   const link = "hover:text-blue-500 hover:bg-blue-100 px-1";
+  const slugify = (text) => text.toLowerCase().replace(/\s+/g, "-")
+  const basePath = "/math/Algebra"
+  const topics = [
+    "Parallel Lines",
+    "Triangle and Trianlge Properties",
+    "Proofs",
+    "Quadrilaterals",
+    "Polygobs",
+    "Circles"
+  ]
   return (
-    <PageWrapper
-      gradient="from-blue-100 to-blue-300"
-      fontColor="text-blue-900"
-    >
+    <PageWrapper gradient="from-blue-100 to-blue-300" fontColor="text-blue-900">
       <div>
         <h1 className="text-5xl text-center tracking-wide py-6">Geometry</h1>
         <div className="flex justify-around">
           <div className="flex flex-col justify-center items-center gap-2">
             <h1 className="text-2xl my-5">TOPICS</h1>
-            <Link href="/linear-equations" className={link}>
-              Linear Equations
-            </Link>
-            <Link href="/linear-equations" className={link}>
-              Linear Equations
-            </Link>
-            <Link href="/linear-equations" className={link}>
-              Linear Equations
-            </Link>
-            <Link href="/linear-equations" className={link}>
-              Linear Equations
-            </Link>
-            <Link href="/linear-equations" className={link}>
-              Linear Equations
-            </Link>
-            <Link href="/linear-equations" className={link}>
-              Linear Equations
-            </Link>
-            <Link href="/linear-equations" className={link}>
-              Linear Equations
-            </Link>
+            {topics.map((topic) => (
+              <Link href={`${basePath}/${slugify(topic)}`} key={topic}>
+                <PreviewCard title={topic} preview={<img src="/file.svg" />}>
+                  <span className={link} preview={<img src="/file.svg" />}>
+                    {topic}
+                  </span>
+                </PreviewCard>
+              </Link>
+            ))}
           </div>
           <div className="flex flex-col items-center gap-2">
             <h1 className="text-2xl my-5">QUICK LINKS</h1>
