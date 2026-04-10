c6f0ede951537fe03619daa26190872edd4fab56
dakshs-07 <dakshpinjore@gmail.com>
2026-04-10 18:57:57 +0530

    fixed preview card bloacking issue

--- components/PreviewCard.jsx
+++ components/PreviewCard.jsx
@@ -5,7 +5,7 @@ function PreviewCard({ title, children, preview}) {
   return (
     <div className="relative group w-fit">
         {children}
-      <div className="absolute left-15 top-full mt-2 w-72 p-4 opacity-0 group-hover:opacity-100 translate-y-2 group-hover:translate-y-0 transition-all duration-200 rounded-2xl shadow-xl bg-white z-50">
+      <div className="absolute left-30 top-full mt-2 w-72 p-4 opacity-0 group-hover:opacity-100 translate-y-2 group-hover:translate-y-0 transition-all duration-200 rounded-2xl shadow-xl bg-white z-50">
         <h4 className="font-semibold text-sm">{title}</h4>
         {preview && <div>{preview}</div>}
       </div>
