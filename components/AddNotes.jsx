d95c6cb17c1c8674b6d35e94a9bc235d2d784360
dakshs-07 <dakshpinjore@gmail.com>
2026-04-10 16:36:22 +0530

    fixed button stylig

--- components/AddNotes.jsx
+++ components/AddNotes.jsx
@@ -5,8 +5,8 @@ export default function AddNotes(){
     return (
       <div className="fixed bottom-3 left-3 z-50">
         <Link href="/admin">
-          <button className="flex text-xl items-center gap-2 bg-amber-400 py-4 px-2 rounded-sm cursor-pointer hover:text-black hover:bg-gray-400">
-            Add Note <PlusIcon size={20} />
+          <button className="flex text-xl tracking-wide items-center gap-2 bg-amber-400 py-4 px-2 rounded-sm cursor-pointer hover:text-black hover:bg-gray-400">
+            UPLOAD NOTES <PlusIcon size={25} />
           </button>
         </Link>
       </div>
